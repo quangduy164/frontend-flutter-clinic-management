c50fd10c554db518713bfe9be784d8ba65516737
quangduy164 <duyhx164@gmail.com>
2024-10-16 10:24:52 +0700

    update specialty

--- lib/ui/home/user_home/home_tab/home_tab.dart
+++ lib/ui/home/user_home/home_tab/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:clinic_management/ui/home/user_home/home_tab/item_service_section.dart';
+import 'package:clinic_management/ui/home/user_home/home_tab/item_specialty_section.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget{
@@ -37,7 +38,75 @@ class HomeTab extends StatelessWidget{
             )
           ],
         ),
-        _getService()
+        _getService(),
+        _getSpecialty()
+      ],
+    );
+  }
+
+  Widget _getSpecialty(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 15, left: 15),
+          child: Text('Chuyên khoa',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14),
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/co-xuong-khop.png',
+                    text: 'Cơ xương khớp'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/than-kinh.png',
+                    text: 'Thần kinh'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/tieu-hoa.png',
+                    text: 'Tiêu hóa'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/tim-mach.png',
+                    text: 'Tim mạch'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/tai-mui-hong.png',
+                    text: 'Tai mũi họng'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/cot-song.png',
+                    text: 'Cột sống'
+                ),
+                const SizedBox(width: 15,),
+                ItemSpecialtySection(
+                    function: (){},
+                    image: 'assets/images/specialty/y-hoc-co-truyen.png',
+                    text: 'Y học cổ truyền'
+                )
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
@@ -60,13 +129,13 @@ class HomeTab extends StatelessWidget{
             children: [
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconkham-chuyen-khoa.png',
+                  image: 'assets/images/services/iconkham-chuyen-khoa.png',
                   text: 'Khám chuyên khoa'
               ),
               const Spacer(),
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconkham-tu-xa.png',
+                  image: 'assets/images/services/iconkham-tu-xa.png',
                   text: 'Khám từ xa'
               ),
             ],
@@ -78,13 +147,13 @@ class HomeTab extends StatelessWidget{
             children: [
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconkham-tong-quat.png',
+                  image: 'assets/images/services/iconkham-tong-quat.png',
                   text: 'Khám tổng quát'
               ),
               const Spacer(),
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconxet-nghiem-y-hoc.png',
+                  image: 'assets/images/services/iconxet-nghiem-y-hoc.png',
                   text: 'Xét nghiệm y học'
               ),
             ],
@@ -96,13 +165,13 @@ class HomeTab extends StatelessWidget{
             children: [
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconsuc-khoe-tinh-than.png',
+                  image: 'assets/images/services/iconsuc-khoe-tinh-than.png',
                   text: 'Sức khỏe tinh thần'
               ),
               const Spacer(),
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconkham-nha-khoa.png',
+                  image: 'assets/images/services/iconkham-nha-khoa.png',
                   text: 'Khám nha khoa'
               ),
             ],
@@ -114,13 +183,13 @@ class HomeTab extends StatelessWidget{
             children: [
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/icongoi-phau-thuat.png',
+                  image: 'assets/images/services/icongoi-phau-thuat.png',
                   text: 'Gói phẫu thuật'
               ),
               const Spacer(),
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconsong-khoe-tieu-duong.png',
+                  image: 'assets/images/services/iconsong-khoe-tieu-duong.png',
                   text: 'Sống khỏe tiểu đường'
               ),
             ],
@@ -132,13 +201,13 @@ class HomeTab extends StatelessWidget{
             children: [
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/iconbai-test-suc-khoe.png',
+                  image: 'assets/images/services/iconbai-test-suc-khoe.png',
                   text: 'Bài test sức khỏe'
               ),
               const Spacer(),
               ItemServiceSection(
                   function: (){},
-                  image: 'assets/images/icony-te-gan-ban.png',
+                  image: 'assets/images/services/icony-te-gan-ban.png',
                   text: 'Y tế gần bạn'
               ),
             ],
---
+++ lib/ui/home/user_home/home_tab/item_specialty_section.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ItemSpecialtySection extends StatefulWidget {
+  final Function? function;
+  final String image;
+  final String text;
+
+  const ItemSpecialtySection(
+      {super.key,
+        required this.function,
+        required this.image,
+        required this.text});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ItemServiceSectionState();
+  }
+}
+
+class _ItemServiceSectionState extends State<ItemSpecialtySection> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        // Kiểm tra xem function có được cung cấp không trước khi gọi
+        if (widget.function != null) {
+          widget.function!(); // Gọi hàm khi được chạm vào
+        }
+      },
+      child: Stack(
+        children: [
+          Container(
+            height: 120,
+            width: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey, // Màu viền
+                width: 1, // Độ dày viền
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    '${widget.image}',
+                    height: 75,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(height: 3),
+                  Expanded(
+                    child: Text(
+                      widget.text,
+                      style: const TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      maxLines: 2, // Giới hạn số dòng chữ
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
